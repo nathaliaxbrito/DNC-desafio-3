c401c8f66aeb5b6bfbc13594944c4c91575c7f55
nathaliaxbrito <nathaliaxbrito@icloud.com>
2022-12-17 14:36:33 -0300

    desafio 3 incompleto

---
+++ script.js
@@ -0,0 +1,38 @@
+var medicamento = [];
+
+function addMedicamento(){
+    event.preventDefault();
+    
+    let remedio = {
+        "nomeRemedio": document.getElementById("nomeMed").value,
+        "quantidade": document.getElementById("qntMed").value,
+        "classe": document.getElementById("classeMed").value,
+    }
+
+    document.getElementById("nomeMed").value = "";
+    document.getElementById("qntMed").value = "";
+    document.getElementById("classeMed").value = "";
+
+    medicamento.push(remedio);
+    renderTable();
+
+}
+
+function renderTable(){
+    let table = document.getElementById(dataTable);
+    table.innerHTML = `<tr>
+    <th>ID</th>
+    <th>Medicamento</th>
+    <th>Quantidade</th>
+    <th>Classe de Medicamento</th>
+    <th>Remover</th>
+
+    </tr>`
+
+for(let i = 0; i < medicamento.length; i++){
+    table.innerHTML = table.innerHTML + `<td>${i}</td>
+    <td>${medicamento[i].nomeMed}</td>
+    <td>${medicamento[i].qntMed}</td>
+    <td>${medicamento[i].classeMed}</td>`
+}
+}
